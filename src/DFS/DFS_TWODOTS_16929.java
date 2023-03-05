5eef8ac40ff970be746da7708ec89cbedead7307
leejunseok <wnstjr120422@naver.com>
2023-03-05 22:42:33 +0900

    DFS_TwoDots 추가

--- src/DFS/DFS_TWODOTS_16929.java
+++ src/DFS/DFS_TWODOTS_16929.java
@@ -6,7 +6,7 @@ public class DFS_TWODOTS_16929 {
     int []dx={-1,1,0,0};
     int []dy={0,0,-1,1};
     int count=0;
-    int x,y;
+    int x,y,s1,s2;
     char s;
     public void run(){
         Scanner sc=new Scanner(System.in);
@@ -15,7 +15,7 @@ public class DFS_TWODOTS_16929 {
         arr=new char[x][y];
         visited=new boolean[x][y];
 
-        for(int i=0;i<x;i++){
+        for(int i=0;i<x;i++){ //배열 입력
             String str=sc.next();
             for(int j=0;j<y;j++){
                 arr[i][j]=str.charAt(j);
@@ -24,35 +24,40 @@ public class DFS_TWODOTS_16929 {
 
         for(int i=0;i<x;i++){
             for(int j=0;j<y;j++){
-                s=arr[i][j];
-                dfs(i,j);
+                if(!visited[i][j]){
+                    visited[i][j]=true;
+                    dfs(i,j,arr[i][j],0);
+                }
+
+                if(count!=0){
+                    System.out.println("Yes");
+                    return;
+                }
+
             }
         }
-
-        if(count==0)
             System.out.println("No");
-        else
-            System.out.println("Yes");
     }
 
-    public void dfs(int index1,int index2){
-
+    public void dfs(int index1,int index2,char ar1,int idx){
 
         for(int i=0;i<4;i++){
             int tmp1=index1+dx[i];
             int tmp2=index2+dy[i];
 
+            if(idx+i==1 || idx+i==5) continue;
             if(tmp1<0||tmp2<0||tmp1>=x||tmp2>=y)
                 continue;
 
-            if(arr[index1][index2]==arr[tmp1][tmp2]&&!visited[tmp1][tmp2]){
-                    if(arr[tmp1][tmp2]==s){
-                        count++;
-                        break;
-                    }
-                visited[tmp1][tmp2]=true;
-                    dfs(tmp1,tmp2);
+
+            if(ar1==arr[tmp1][tmp2]){
+                if(visited[tmp1][tmp2]){
+                    count++;
+                    return;
                 }
+                visited[tmp1][tmp2]=true;
+                    dfs(tmp1,tmp2,arr[tmp1][tmp2],i);
+            }
 
         }
     }
