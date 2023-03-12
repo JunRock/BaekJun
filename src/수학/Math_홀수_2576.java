a148d9538bf082497ccf102ac0900b585cb82c6f
leejunseok <wnstjr120422@naver.com>
2023-03-12 19:03:47 +0900

    수학 홀수 문제 추가

---
+++ src/수학/Math_홀수_2576.java
@@ -0,0 +1,40 @@
+package 수학;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.*;
+public class Math_홀수_2576 {
+    public void run(){
+        Scanner sc=new Scanner(System.in);
+        int x;
+        int k=0;
+        int sum=0;
+        int []tmp=new int[7];
+        for(int i=0;i<7;i++){
+            x=sc.nextInt();
+            tmp[i]=x;
+            if(x%2!=0)
+               ++k;
+        }
+
+        int []arr=new int[k];
+        k=0;
+        for(int i=0;i<7;i++){
+            if(tmp[i]%2!=0)
+                arr[k++]=tmp[i];
+        }
+
+        int min1=arr[0];
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            min1= Math.min(min1,arr[i]);
+        }
+
+        System.out.println(sum);
+        System.out.println(min1);
+    }
+
+
+    public static void main(String[] args){
+        Math_홀수_2576 math_홀수_2576=new Math_홀수_2576();
+        math_홀수_2576.run();
+    }
+}
