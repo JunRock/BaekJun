e064d2a383ef35436982fb9e5a8e7cde4fa454d6
leejunseok <wnstjr120422@naver.com>
2023-06-24 23:44:51 +0900

    [add]: STRING_Alpabet_2941

---
+++ src/문자열/STRING_Alpabet_2941.java
@@ -0,0 +1,16 @@
+package 문자열;
+import java.util.*;
+public class STRING_Alpabet_2941 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[]str={"c=","c-","dz=","d-","lj","nj","s=","z="};
+        String s=sc.next();
+
+        for(int i=0;i<s.length();i++){
+            if(s.contains(str[i])){
+                s=s.replace(str[i],"0");
+            }
+        }
+        System.out.println(s.length());
+    }
+}
