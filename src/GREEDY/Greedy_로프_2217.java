9a7c5f933ed481bd8e501ff7801bd1efb423f69d
leejunseok <wnstjr120422@naver.com>
2023-11-06 17:52:43 +0900

    fix: add Greedy_로프_2217

---
+++ src/GREEDY/Greedy_로프_2217.java
@@ -0,0 +1,23 @@
+package GREEDY;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Greedy_로프_2217 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int[]arr=new int[size];
+
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int maxWeight=0;
+        for(int i=0;i<size;i++){
+            int weight=arr[i]*(size-i);
+            maxWeight=Math.max(weight,maxWeight);
+        }
+        System.out.println(maxWeight);
+    }
+}
