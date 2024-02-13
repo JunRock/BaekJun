9915c1e8d9ada55cd0914c4c5a04634562b00af2
leejunseok <wnstjr120422@naver.com>
2024-02-13 21:05:26 +0900

    feat : 구현_달팽이_1913

---
+++ src/구현/구현_Snail_1913.java
@@ -0,0 +1,56 @@
+package 구현;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class 구현_Snail_1913 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int expectNum=sc.nextInt();
+        int [][]map=new int[size][size];
+        int num=size*size;
+        int top=size-1;
+        int bottom=0;
+        int left=0;
+        int right=size;
+
+        while(num>0){
+           for(int i=bottom;i<=top;i++){ //아래로 내려가는 경우
+               map[i][left]=num--;
+           }
+           left++;
+
+           for(int i=left;i<right;i++){ //오른쪽으로 가는 경우
+               map[top][i]=num--;
+           }
+           top--;
+
+           for(int i=top;i>bottom;i--){ //위쪽으로 가는 경우
+               map[i][right-1]=num--;
+           }
+           right--;
+
+           for(int i=right;i>=left;i--){ //왼쪽으로 가는 경우
+               map[bottom][i]=num--;
+           }
+            bottom++;
+        }
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(map[i][j]==expectNum){
+                    System.out.print((i+1)+" "+(j+1));
+                    return;
+                }
+            }
+        }
+    }
+}
