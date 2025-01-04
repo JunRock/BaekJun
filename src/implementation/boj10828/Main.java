bb439c7cd15669757d6433558f693322c7772e08
JunRock <wnstjr120422@naver.com>
2025-01-04 22:57:32 +0900

    feat : solve 10828

---
+++ src/implementation/boj10828/Main.java
@@ -0,0 +1,63 @@
+package implementation.boj10828;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+
+    private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        int n = Integer.parseInt(read());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] str = read().split(" ");
+            String command = str[0];
+            int number = 0;
+            if (command.equals("push")) {
+                number = Integer.parseInt(str[1]);
+            }
+            switch (command) {
+                case "push":
+                    stack.add(number);
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+            }
+        }
+    }
+
+    private static String read() {
+        try {
+            return bf.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
