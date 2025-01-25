3503d2ae7edbf759e9b71826928fff5e64a3a204
JunRock <wnstjr120422@naver.com>
2025-01-25 23:55:21 +0900

    feat : solve 11652

--- src/sorting/boj11652/Main.java
+++ src/sorting/boj11652/Main.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
-
 public class Main {
 
     private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
