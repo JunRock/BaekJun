b0694765c74fc83b86cf5314f40d85a60e165215
JunRock <wnstjr120422@naver.com>
2025-02-02 20:00:37 +0900

    feat : solve 1339

--- src/GREEDY/boj1339/Main.java
+++ src/GREEDY/boj1339/Main.java
@@ -13,7 +13,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.jetbrains.annotations.NotNull;
 
 public class Main {
 
