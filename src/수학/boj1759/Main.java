45f74de6cbe317b49dd5bb93a4fc09c2db86def8
JunRock <wnstjr120422@naver.com>
2025-02-04 16:26:35 +0900

    feat : solve 1759

--- src/수학/boj1759/Main.java
+++ src/수학/boj1759/Main.java
@@ -10,11 +10,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static int l,c;
+    private static int l, c;
     private static String[] word;
     private static List<String> result = new ArrayList<>();
+
     public static void main(String[] args) {
         String[] input = readWithSplit();
         l = Integer.parseInt(input[0]);
@@ -22,14 +24,14 @@ public class Main {
         word = readWithSplit();
         Arrays.sort(word);
 
-        backTrack(0,0,"",0,0);
-       write(result);
-       close();
+        backTrack(0, 0, "", 0, 0);
+        write(result);
+        close();
     }
 
-    private static void backTrack(int start, int length, String current,int vowel, int consonant){
-        if (length==l){
-            if (vowel>=1&&consonant>=2){
+    private static void backTrack(int start, int length, String current, int vowel, int consonant) {
+        if (length == l) {
+            if (vowel >= 1 && consonant >= 2) {
                 result.add(current);
             }
             return;
@@ -37,43 +39,43 @@ public class Main {
 
         for (int i = start; i < c; i++) {
             String str = word[i];
-            if(isVowel(str)){
-                backTrack(i+1,length+1,current+str,vowel+1,consonant);
-            }else{
-                backTrack(i+1,length+1,current+str,vowel,consonant+1);
+            if (isVowel(str)) {
+                backTrack(i + 1, length + 1, current + str, vowel + 1, consonant);
+            } else {
+                backTrack(i + 1, length + 1, current + str, vowel, consonant + 1);
             }
         }
     }
 
-    private static boolean isVowel(String str){
+    private static boolean isVowel(String str) {
         return "aeiou".contains(str);
     }
 
-    private static String[] readWithSplit(){
+    private static String[] readWithSplit() {
         try {
             return br.readLine().split(" ");
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void write(List<String> result){
+    private static void write(List<String> result) {
         try {
-            for (String str : result){
+            for (String str : result) {
                 bw.write(str);
                 bw.newLine();
             }
             bw.flush();
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void close(){
+    private static void close() {
         try {
             bw.close();
             br.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
